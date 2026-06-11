4fe69f03d64fb80333e751425a0135130f25bf97
surajpatel9135 <patelsuraj62068@gmail.com>
2026-06-12 00:01:17 +0530

    checks number is happy number return true otherwise return false.

---
+++ HappyNum.java
@@ -0,0 +1,55 @@
+// LeetCode Problem - 202
+// Write an algorithm to determine if a number n is happy.
+
+// A happy number is a number defined by the following process:
+
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+ 
+
+// Example 1:
+
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+// Example 2:
+
+// Input: n = 2
+// Output: false
+
+
+import java.util.*;
+
+class HappyNum {
+    public static boolean isHappy(int n) {
+ 
+        while(n >= 5){
+            n = sqr(n);
+        }
+        
+        return n==1;
+    }
+
+    public static int sqr(int n){
+        int result = 0;
+        for(int i = n; i > 0; i/=10){
+            result += Math.pow(i%10, 2);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Happy Number: "+isHappy(n));
+    }
+}
